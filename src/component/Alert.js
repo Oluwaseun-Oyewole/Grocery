041ee9b4a67df4befdddd9ec93ea55fc8a223374
seunsamueldcompguy <seun.samuel.dcomputer.guy@gmail.com>
2021-03-04 23:24:30 -0930

    app, list.js

---
+++ src/component/Alert.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React, { useEffect } from "react";
+
+const Alert = () => {
+  return <h2>alert component</h2>;
+};
+
+export default Alert;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./component/App";
+import "./component/index.css";
+ReactDOM.render(
+  <App></App>,
+  document.getElementById("root")
+);
